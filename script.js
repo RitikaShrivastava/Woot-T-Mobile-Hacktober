03770e9bc97854a252efd196c73d6b21b450183f
RitikaShrivastava <ritika.shrivastava@utdallas.edu>
2017-10-22 05:50:17 -0500

    changes

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ $(document).ready(function(){
             url: 'https://1eed0w5fyb.execute-api.us-west-2.amazonaws.com/prod/wootRestEndpoint?TableName=TruckInfo',
             dataType: 'json',
             success: function(response) {
-                console.log("%o", response)
+                console.log("response");
+            }, 
+            error: function(error) {
+            	console.log(error);
             }
         });
     }, 5000);
